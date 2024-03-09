1d18cc5c1d8b7efa39d7f42c25d618c59b2777e4
unknown <alcp6201@gmail.com>
2024-03-09 17:30:57 -0500

    added button to trigger happy

--- horizons/pet.py
+++ horizons/pet.py
@@ -3,6 +3,7 @@ from PIL import Image, ImageTk
 import os
 
 class VirtualPet(tk.Tk):
+
     def __init__(self):
         super().__init__()
 
@@ -19,12 +20,10 @@ class VirtualPet(tk.Tk):
         self.attributes("-transparentcolor", "white")
 
         # Load the image for the pet
-        self.pet_images = self.load_pet_images()
+        self.state = "idle"
+        self.pet_images = self.load_pet_images(self.state)
         self.current_image_index = 0
         self.pet_image = self.pet_images[self.current_image_index]
-        
-        # Set the window size based on the pet image size //make window size = desktop size
-        #self.geometry(f"{self.pet_image.width()}x{self.pet_image.height()}")
 
         # Create a canvas to hold the pet image
         self.canvas = tk.Canvas(self, width=width, height=height, bg='white', highlightthickness=0)
@@ -37,16 +36,26 @@ class VirtualPet(tk.Tk):
         self.canvas.bind("<Button-1>", self.start_drag)
         self.canvas.bind("<B1-Motion>", self.dragging)
 
+        self.interact_button = tk.Button(self, text="Interact", command=self.interact_with_pet, bg='white')
+        self.interact_button.place(relx=0.5, rely=0.9, anchor=tk.CENTER)
+
         # Schedule updating the pet image
         self.update_pet_image()
 
-    def load_pet_images(self):
+    def interact_with_pet(self):
+        self.state = "happy"
+        self.pet_images = self.load_pet_images(self.state)
+        self.current_image_index = 0
+        print("Trying to be happy...")
+        return
+
+    def load_pet_images(self, name):
         pet_images = []
         # Provide the directory path where your pet animation frames are located
         current_directory = os.path.dirname(__file__)
 
         # Navigate to the images folder using the relative path
-        animation_folder = os.path.join(current_directory, "animation_frames", "idle")
+        animation_folder = os.path.join(current_directory, "animation_frames", name)
         # animation_folder = "\\animation_frames"
         for filename in sorted(os.listdir(animation_folder)):
             image_path = os.path.join(animation_folder, filename)
@@ -58,6 +67,7 @@ class VirtualPet(tk.Tk):
             pet_images.append(image)
         return pet_images
 
+
     def update_pet_image(self):
         # Update the pet image on the canvas
         self.pet_image = self.pet_images[self.current_image_index]
@@ -67,7 +77,7 @@ class VirtualPet(tk.Tk):
         self.current_image_index = (self.current_image_index + 1) % len(self.pet_images)
 
         # Schedule the next update after a certain delay (in milliseconds)
-        self.after(200, self.update_pet_image)
+        self.after(150, self.update_pet_image)
 
     def start_drag(self, event):
         # Record the starting position of the mouse when dragging starts
