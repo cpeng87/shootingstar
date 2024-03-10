d764d7594c784d430020c9154efaac6b495159e1
Shinyoung Park <122953071+parkelly@users.noreply.github.com>
2024-03-09 19:43:44 -0500

    Updated exit button again

--- horizons/pet.py
+++ horizons/pet.py
@@ -45,14 +45,14 @@ class VirtualPet(tk.Tk):
         # creates right click pop-up, floating menu
         self.menu = tk.Menu(self, tearoff=0)
         self.menu.add_command(label = "Exit", command = self.quit)
-        self.menu.add_separator()
-
-        def popup(e):
-            self.menu.tk_popup(e.x, e.y)   
-
-        self.bind("<Button-3>", popup)
+        #self.menu.add_separator()
+        self.bind("<Button-3>", self.popup)
 
 
+
+
+    def popup(self, e):
+        self.menu.tk_popup(e.x, e.y)   
 
     def interact_with_pet(self):
         self.state = "happy"
